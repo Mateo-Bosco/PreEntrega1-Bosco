060689de29bd08d014ee2e83cb63604ae540fe2a
Bosco <mateobosco@outlook.com>
2024-02-07 20:08:47 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css"
+import { Navbar } from "./Navbar/Navbar"
+import { Footer } from "./Footer/Footer"
+import { Main } from "./Main/Main"
+
+function App() {
+
+  return (
+    <div>
+      <Navbar/>
+      <Main/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,10 @@
+import "./Navbar.css"
+export const Navbar = ()=>{
+    return <nav>
+        <ul>
+            <li>ma</li>
+            <li>map</li>
+            <li>mal</li>
+        </ul>
+    </nav>
+}
