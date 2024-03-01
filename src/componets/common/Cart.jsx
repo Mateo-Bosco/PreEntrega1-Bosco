42c46380b873b73b04abce422810e71c68401918
Bosco <mateobosco@outlook.com>
2024-03-01 11:58:51 -0300

    Arreglos esteticos

--- src/componets/common/Cart.jsx
+++ src/componets/common/Cart.jsx
@@ -1,6 +1,9 @@
 
 export const Cart = () => {
     return (
-        <h1>cart</h1>
+        <div className="div-cart">
+            <h1>Carrito en mantenimiento</h1>
+            <img className="cart-img" src="https://res.cloudinary.com/dkekabdhg/image/upload/v1709304871/e50c7462948db5418fb3087ae1b34032_aitv8v.jpg" alt="" />
+        </div>
     )
 }
